b5d208df18290d20920bc5b81b0b683b7ff00c9a
Omar <omar.assed@gmail.com>
2022-05-21 08:55:03 -0600

    commit tree 27bc49ee01e8e426d8d7fa670b0e33942ccb62d3

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+    return [array[0], array[1]];
+}
+const returnLastTwoDrivers = function(array) {
+    return [array[array.length-2], array[array.length-1]];
+}
